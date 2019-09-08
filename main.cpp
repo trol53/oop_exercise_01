4ac26238a4cf5e1180282f32fd8d955a548c899b
trol53 <nek.savrik@yandex.ru>
2019-09-08 10:53:09 +0300

    Wrote class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <cmath>
 
 using namespace std;
 
@@ -23,16 +24,52 @@ private:
      house = name3;
      apartment = name4;
    }
+   void Set(){
+     cin >> city >> street >> house >> apartment;
+   }
    bool operator ==(Address a){
-       transform(a.city.begin(), a.city.end(), a.city.begin(), tolower);
-       transform(this->city.begin(), this->city.end(), this->city.begin(), tolower);
+       transform(a.city.begin(), a.city.end(), a.city.begin(), (int (*)(int))tolower);
+       transform(this->city.begin(), this->city.end(), this->city.begin(), (int (*)(int))tolower);
+       transform(a.street.begin(), a.street.end(), a.street.begin(), (int (*)(int))tolower);
+       transform(this->street.begin(), this->street.end(), this->street.begin(), (int (*)(int))tolower);
        return a.city == this->city && a.street == this->street && a.house == this->house && a.apartment == this->apartment;
    }
-   //bool
+   bool is_city(string town){
+     transform(town.begin(), town.end(), town.begin(), (int (*)(int))tolower);
+     transform(this->city.begin(), this->city.end(), this->city.begin(), (int (*)(int))tolower);
+     return this->city == town;
+   }
+   bool is_street(string stree){
+     transform(stree.begin(), stree.end(), stree.begin(), (int (*)(int))tolower);
+     transform(this->street.begin(), this->street.end(), this->street.begin(), (int (*)(int))tolower);
+     return this->street == stree;
+   }
+   bool is_neighbour(Address b){
+     transform(a.city.begin(), a.city.end(), a.city.begin(), (int (*)(int))tolower);
+     transform(this->city.begin(), this->city.end(), this->city.begin(), (int (*)(int))tolower);
+     transform(a.street.begin(), a.street.end(), a.street.begin(), (int (*)(int))tolower);
+     transform(this->street.begin(), this->street.end(), this->street.begin(), (int (*)(int))tolower);
+     if (b.city == this->city && b.street == this->street){
+       if (abs(b.house - this->house) == 1){
+         return true;
+       }
+     }
+     return false;
+   }
 };
 
 int main(){
-  Address *a = new Address();
-  Address *b = new Address("MoscoW", "Lenina", 10, 10);
-  cout << (*a == *b);
+  // Address *a = new Address();
+  // Address *b = new Address("MoscoW", "Lenina", 10, 15);
+  // a->Set();
+  // b->Set();
+  // cout << (*a == *b);
+  // string town,stree;
+  // cout << "Enter street\n";
+  // cin >> stree;
+  // cout << a->is_street(stree);
+  // cout << "Enter city\n";
+  // cin >> town;
+  // cout << a->is_city(town);
+  
 }
